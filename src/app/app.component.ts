a4f5cfbbc63f976aa794888018d19d1a8889f119
JustinPark180 <64935915+JustinPark180@users.noreply.github.com>
2020-11-06 21:47:18 -0500

    11/6/2020

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Two from '../assets/two.min.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void{
+    let elem = document.getElementById('draw-shapes');
+    let parmas = {fullscreen: true};
+    let two = new Two(parmas).appendTo(elem);
+
+    let circle=two.makeCircle(72, 100, 50);
+    circle.fill = "#FF8000";
+    let rect = two.makeRectangle(213, 100, 100, 100);
+    
+
+    two.update();
+  }
+
+  
   title = 'NewGame';
-}
+
+  }
