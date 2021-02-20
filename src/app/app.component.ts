79aa385687cc2f0830d3de2d291ebabf6036a3c2
Seojin Park <sjparkish@gmail.com>
2021-02-20 12:06:10 +0900

    2/20/2021

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { HostListener } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 import Two from '../assets/two.min.js';
 import { AiService } from './services/ai.service.js';
-import { CameraService } from './services/camera.service.js';
+import { CameraService } from './services/camera.service';
 import { CollisionService } from './services/collision.service.js';
 import { MapService } from './services/map.service.js';
-import { Sprite, SpriteService } from './services/sprite.service.js';
+import { SpriteService } from './services/sprite.service';
+
 
 @Component({
   selector: 'app-root',
@@ -13,19 +14,19 @@ import { Sprite, SpriteService } from './services/sprite.service.js';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title(title: any) {
-    throw new Error('Method not implemented.');
-  }
   direction:string;
   
   x: number=200;
   y: number=200;
 
-  max_x: number= 3500;
-  max_y: number= 2500;
+  max_x: number= 1500;
+  max_y: number= 1300;
 
-
-  constructor(private _spriteService: SpriteService, private _cameraService: CameraService, private _mapService: MapService, private _aiservice: AiService, private _collisionService: CollisionService) {}
+  constructor(private _spriteService: SpriteService, 
+    private _cameraService: CameraService, 
+    private _aiService: AiService, 
+    private _mapService: MapService,
+    private _collisionService: CollisionService) {}
 
   @HostListener('document:keydown', ['$event'])
   handleKey(event: any) {
@@ -50,40 +51,49 @@ export class AppComponent implements OnInit {
     let elem = document.getElementById('map');
     let params = {
       width: this._mapService.MAX_X,
-      height: this._mapService.MAX_Y 
+      height: this._mapService.MAX_Y
+
     };
     let two = new Two(params).appendTo(elem);
-    
-    this._mapService.init(two)
+
+    this._mapService.init(two);
     this._cameraService.init(this.max_x, this.max_y);
     this._spriteService.populateCake(10)
     this._spriteService.populateTree(10)
-    //loop through service
+    
+   
+
     for (let i=this._spriteService.sprites.length-1; i>=0; i--) {
       let sprite=this._spriteService.sprites[i];
       this._spriteService.sprites[i].sprite=two.makeSprite(sprite.url, sprite.x, sprite.y, sprite.columns, sprite.rows, sprite.fps);
       this._spriteService.sprites[i].sprite.play(this._spriteService.sprites[i].rightFrames[0], this._spriteService.sprites[i].rightFrames[1]);
-      //this._spriteService.sprites[i].sprite.scale=this._spriteService.sprites[i].scale;
+      this._spriteService.sprites[i].sprite.scale=this._spriteService.sprites[i].scale;
     }
-    console.log(this._spriteService.sprites)
-    //rectangle.scale=.7;
+    
     two.bind('update', (framesPerSecond)=>{
-      // this is where animatoin happens
+    
 
-
-      if (this.x+70<this.max_x && this.x-70>0) this._spriteService.sprites[0].sprite.translation.x=this.x;
-      if (this.y+30<this.max_y && this.y-70>0) this._spriteService.sprites[0].sprite.translation.y=this.y;
+    
+      if (this.x+60<this.max_x && this.x-70>0) {
+        this._spriteService.sprites[0].sprite.translation.x=this.x;
+        this._spriteService.sprites[0].x = this.x;
+      } 
+      if (this.y+20<this.max_y && this.y-70>0) {
+        this._spriteService.sprites[0].sprite.translation.y=this.y;
+        this._spriteService.sprites[0].y = this.y;
+      } 
       
       this._cameraService.zoomCamera(this.x, this.y);
-
-        for (let i=this._spriteService.sprites.length-1; i>=0; i--) {
+        for (let i= this._spriteService.sprites.length-1; i>=0; i--) {
           if (i>0) {
             if (!this._spriteService.sprites[i]) continue
-            this._spriteService.sprites[i]=this._aiservice.basicAI(this._spriteService.sprites[i]);
+            this._spriteService.sprites[i] = this._aiService.basicAI(this._spriteService.sprites[i]);
             this._spriteService.sprites[i].sprite.translation.x = this._spriteService.sprites[i].x;
             this._spriteService.sprites[i].sprite.translation.y = this._spriteService.sprites[i].y;
-            this._spriteService.sprites[i].sprite.scale = this._spriteService[i].scale;
+            this._spriteService.sprites[i].sprite.scale = this._spriteService.sprites[i].scale;
+            this._collisionService.detectCollision(this._spriteService.sprites[0], this._spriteService.sprites[i]);
           }
+        
           if (this._spriteService.sprites[i].direction != this._spriteService.sprites[i].lastDirection) {
             this._spriteService.sprites[i].lastDirection=this._spriteService.sprites[i].direction;
             if (this._spriteService.sprites[i].direction=='right') {
@@ -98,8 +108,7 @@ export class AppComponent implements OnInit {
     }).play();
   }
 
-  
+  title = 'Game';
 
-  
 
 }
--- src/app/services/collision.service.ts
+++ src/app/services/collision.service.ts
@@ -5,33 +5,38 @@ import { Sprite } from "./sprite.service";
     providedIn: 'root'
 })
 export class CollisionService {
+    private _mapService: any;
 
     constructor() { }
 
     detectCollision(mySprite: Sprite, targetSprite: Sprite) {
-        const OFFSET = 2;
-
+        const OFFSET = 4;
+    
         let width=mySprite.sprite.width;
-        let height=mySprite.sprite.height;
-
+        let height = mySprite.sprite.height;
+        
         let leftBound = mySprite.x-(width/OFFSET);
-        let rightBound = mySprite.x-(width/OFFSET);
-        let upperBound = mySprite.x-(height/OFFSET);
-        let lowerBound = mySprite.x-(height/OFFSET);
+        let rightBound = mySprite.x+(width/OFFSET);
+        let upperBound = mySprite.y-(height/OFFSET);
+        let lowerBound = mySprite.y+(height/OFFSET);
         width = targetSprite.sprite.width;
         height = targetSprite.sprite.height;
-
+    
         let targetLeftBound = targetSprite.x-(width/OFFSET);
-        let targetRightBound = targetSprite.x-(width/OFFSET);
-        let targetUpperBound = targetSprite.x-(height/OFFSET);
-        let targetLowerBound = targetSprite.x-(height/OFFSET);
-
-        if ((leftBound<targetLeftBound && targetLeftBound<rightBound)
+        let targetRightBound = targetSprite.x+(width/OFFSET);
+        let targetUpperBound = targetSprite.y-(height/OFFSET);
+        let targetLowerBound = targetSprite.y+(height/OFFSET);
+    
+        if ((leftBound<targetLeftBound && targetLeftBound<rightBound) 
         || (leftBound<targetRightBound && targetRightBound<rightBound)) {
-            if ((upperBound<targetUpperBound && targetUpperBound<lowerBound)
-            || (upperBound<targetLowerBound && targetLowerBound<lowerBound)) {
-                targetSprite.scale=0;
-            }
+          //console.log('horizontal check!')
+          if ((upperBound<targetUpperBound && targetUpperBound<lowerBound) 
+          || (upperBound<targetLowerBound && targetLowerBound<lowerBound)) {
+    
+              targetSprite.scale = 0
+    
+    
+          }
         }
-    }
+      }    
 }
